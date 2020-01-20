0d30298afe8c8e0e1f78312250f757773c3ee570
Abdullah Karagöz <abdullahkaragoz30@gmail.com>
2020-01-21 01:32:53 +0300

    FirstCommit

---
+++ AjaxExampleApp/Controllers/AjaxUsageController.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using System.Web.Mvc;
+
+namespace AjaxExampleApp.Controllers
+{
+    public class AjaxUsageController : Controller
+    {
+        // GET: AjaxUsage
+        public ActionResult Index()
+        {
+            return View();
+        }
+       
+        [HttpPost]
+        public ActionResult GetValueContent()
+        {
+            return Content("Merhaba bu ajax için oluşturulmuş bir action'dır.");
+        }
+        
+        [HttpPost]
+        public ActionResult GetValueView()
+        {
+            return View("Test");
+        }
+      
+        [HttpPost]
+        public ActionResult GetBadRequest()
+        {
+            return new HttpStatusCodeResult(HttpStatusCode.GatewayTimeout);
+        }
+    }
+}
